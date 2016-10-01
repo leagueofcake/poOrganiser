8d0f0239a18fcc6978cb22260b25640e8a2009a2
Dominic Fung <domokun997@gmail.com>
2016-10-01 11:13:49 +1000

    Add getters and unit tests for User class

--- User.py
+++ User.py
@@ -6,3 +6,41 @@ class User():
 
     def get_name(self):
         return self.name
+
+    def get_going(self):
+        return self.going
+
+    def get_roles(self):
+        return self.roles
+
+
+def run_tests():
+    test_get_name()
+    test_get_going()
+    test_get_roles()
+
+def test_get_name():
+    u1 = User("") # Empty case
+    assert(u1.get_name() == "")
+
+    u1 = User(" ") # Whitespace case
+    assert(u1.get_name() == " ")
+
+    u1 = User("Jeremy") # Whitespace case
+    assert(u1.get_name() == "Jeremy")
+
+def test_get_going():
+    u1 = User("")
+    assert(u1.get_going() == False)
+
+    u1 = User("Jeremy")
+    assert(u1.get_going() == False)
+
+def test_get_roles():
+    u1 = User("")
+    assert(u1.get_roles() == [])
+
+    u1 = User("Jeremy")
+    assert(u1.get_roles() == [])
+
+run_tests()

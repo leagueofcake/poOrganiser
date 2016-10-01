77f3cd3a7dc1fb2b1406836c81d843c2bd3f9cd2
Jeremy Ng <jeremykyng@gmail.com>
2016-10-01 11:16:45 +1000

    basic cli working

--- porg_cli.py
+++ porg_cli.py
@@ -1,1 +1,81 @@
 #!/usr/bin/env python3.5
+import re
+from Event import *
+from User import *
+
+def help():
+    print("Usage: event_name action arguments")
+    print("Examples:")
+    print("\tevent_name create location date")
+    print("\tevent_name add user user_name")
+    print("\tevent_name get location")
+    print("\tevent_name edit location ")
+    print("EXIT: enter nothing to exit")
+    print("DEBUGGING COMMANDS:")
+    print("\tprint: prints all events")
+
+events = {}
+while True:
+    userInput = input()
+    if not userInput or userInput.lower() == "exit":
+        print("Goodbye.")
+        break
+    elif userInput == "help":
+        help()
+        continue
+    elif userInput == "print":
+        # TO DO
+        print("DEBUGGING OUTPUT (TO DO)")
+        continue
+    splits = userInput.split(' ')
+    event_name = splits[0]
+    cmd = splits[1].lower()
+    if cmd == "create":
+        found = False
+        if event_name in events:
+            print("Event already exists")
+        else:
+            if len(splits) != 4:
+                print("Incorrect number of arguments, correct usage: event_name create location time")
+            location = splits[2]
+            time = splits[3]
+            new_event = Event(event_name, location, time)
+            events[event_name] = new_event
+            print("New event %s created, at %s on %s" % (event_name, location, time))
+    elif event_name in events:
+        curr_event = events[event_name]
+        if cmd == "add":
+            new_type = splits[2].lower()
+            if new_type == "user":
+                user_name = splits[3]
+                if not user_name:
+                    print("Incorrect number of arguments, correct usage: event_name add type name")
+                elif not curr_event.get_attendee(user_name):
+                    curr_event.add_attendee(user_name)
+                    print("Added new attendee %s to %s" % (user_name, event_name))
+            else:
+                print("Unknown type")
+        elif cmd == "get":
+            get_type = splits[2].lower()
+            if get_type == "location":
+                print(curr_event.get_location())
+            elif get_type == "time":
+                print(curr_event.get_time())
+            else:
+                print("Unknown type")
+        elif cmd == "edit":
+            set_type = splits[2].lower()
+            edit_value = splits[3].lower()
+            if not edit_value:
+                print("Incorrect number of arguments, correct usage: event_name edit type value")
+            else:
+                if get_type == "location":
+                    curr_event.set_location(edit_value)
+                    print("%s's location updated to %s" % (event_name, edit_value))
+                elif get_type == "time":
+                    curr_event.set_time(edit_value)
+                    print("%s's time updated to %s" % (event_name, edit_value))
+                else:
+                    print("Unknown type")
+    else:
+        print("Event not found")

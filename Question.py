911d214794919119f5e1f9f2595769e8c4b060e4
Dennis Lee <thegodofflash7@hotmail.com>
2016-10-01 12:51:30 +1000

    Add tests to Question

--- Question.py
+++ Question.py
@@ -18,3 +18,50 @@ class Question():
 
     def set_text(self, text):
         self.question_text = text
+
+ #tests
+
+def run_tests():
+    test_get_text()
+    test_set_text()
+    test_add_option()
+    test_get_options()
+    
+
+def test_get_text():
+    q1 = Question("") # Empty case
+    assert(q1.get_text() == "")
+
+    q1 = Question(" ") # Whitespace case
+    assert(q1.get_text() == " ")
+
+    q1 = Question("Do you lickadickaday?") # normal case
+    assert(q1.get_text() == "Do you lickadickaday?")
+
+
+def test_set_text():
+    q1 = Question("test")
+    q1.set_text("Do you lickadickaday?")
+    assert(q1.get_text() == "Do you lickadickaday?")
+    q1.set_text(" ")
+    assert(q1.get_text() == " ")
+    q1.set_text("")
+    assert(q1.get_text() == "")
+
+def test_add_option():
+    q1 = Question("test")
+    assert(q1.get_options() == {})
+    q1.add_option("Yes")
+    assert(q1.get_options()["Yes"] == 0)
+    q1.add_option("No")
+    assert(q1.get_options()["No"] == 0)
+
+def test_get_options():
+    q1 = Question("")
+    assert(q1.get_options() == {})
+
+    q1 = Question("test")
+    assert(q1.get_options() == {})
+    
+    
+run_tests()

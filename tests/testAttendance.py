31c6adf1c81bc4f1d8a42ee6823718058de108e4
Dominic Fung <domokun997@gmail.com>
2017-01-16 10:41:53 +0800

    Attendance: add tests for get_user_id, get_event_id, update other tests

--- tests/testAttendance.py
+++ tests/testAttendance.py
@@ -4,68 +4,87 @@ from Poorganiser import Attendance
 
 
 class TestAttendance(unittest.TestCase):
+    def test_get_user_id(self):
+        at = Attendance(4, 2)
+        self.assertEqual(at.get_user_id(), 4)
+        at = Attendance(999, 1000)
+        self.assertEqual(at.get_user_id(), 999)
+        at = Attendance(1234, 2345)
+        self.assertEqual(at.get_user_id(), 1234)
+
+    def test_get_event_id(self):
+        at = Attendance(4, 2)
+        self.assertEqual(at.get_event_id(), 2)
+        at = Attendance(999, 1000)
+        self.assertEqual(at.get_event_id(), 1000)
+        at = Attendance(1234, 2345)
+        self.assertEqual(at.get_event_id(), 2345)
+
     def test_get_going_status(self):
-        at1 = Attendance(4, 2)
-        self.assertEqual(at1.get_going_status(), "invited")
+        at = Attendance(4, 2)
+        self.assertEqual(at.get_going_status(), "invited")
 
-        at1 = Attendance(1, 2, "invited", [])
-        self.assertEqual(at1.get_going_status(), "invited")
+        at = Attendance(194, 37, " ", [])
+        self.assertEqual(at.get_going_status(), " ")
 
-        at1 = Attendance(1, 2, "Going", [])
-        self.assertEqual(at1.get_going_status(), "Going")
+        at = Attendance(1, 2, "invited", [])
+        self.assertEqual(at.get_going_status(), "invited")
 
-        at1 = Attendance(1, 2, "Not Going", [])
-        self.assertEqual(at1.get_going_status(), "Not Going")
+        at = Attendance(1, 2, "going", [])
+        self.assertEqual(at.get_going_status(), "going")
+
+        at = Attendance(1, 2, "not_going", [])
+        self.assertEqual(at.get_going_status(), "not_going")
 
     def test_get_roles(self):
-        at1 = Attendance(5, 10)
-        self.assertEqual(at1.get_roles(), [])
+        at = Attendance(5, 10)
+        self.assertEqual(at.get_roles(), [])
 
-        at1 = Attendance(32, 198, True, [])
-        self.assertEqual(at1.get_roles(), [])
+        at = Attendance(32, 198, True, [])
+        self.assertEqual(at.get_roles(), [])
 
-        at1 = Attendance(48, 3230, False, ['bring food'])
-        self.assertEqual(at1.get_roles(), ['bring food'])
+        at = Attendance(48, 3230, False, ['bring food'])
+        self.assertEqual(at.get_roles(), ['bring food'])
 
-        at1 = Attendance(1266, 987, True, ['bring food', 'drive'])
-        self.assertEqual(at1.get_roles(), ['bring food', 'drive'])
+        at = Attendance(1266, 987, True, ['bring food', 'drive'])
+        self.assertEqual(at.get_roles(), ['bring food', 'drive'])
 
     def test_set_going_status(self):
-        at1 = Attendance(1, 1)
-        self.assertEqual(at1.get_going_status(), "invited")
-        at1.set_going_status("Going")
-        self.assertEqual(at1.get_going_status(), "Going")
-        at1 = Attendance(43, 103, "Not Going")
-        self.assertEqual(at1.get_going_status(), "Not Going")
+        at = Attendance(1, 1)
+        self.assertEqual(at.get_going_status(), "invited")
+        at.set_going_status("going")
+        self.assertEqual(at.get_going_status(), "going")
+        at = Attendance(43, 103, "not_going")
+        self.assertEqual(at.get_going_status(), "not_going")
 
     def test_add_role(self):
-        at1 = Attendance(5, 10, True, [])
-        at1.add_role('bring food')
-        self.assertEqual(at1.get_roles(), ['bring food'])
-        at1.add_role('drive')
-        self.assertEqual(at1.get_roles(), ['bring food', 'drive'])
-        at1 = Attendance(1032, 39281, False, ['Book venue'])
-        at1.add_role('do something')
-        at1.add_role('blah')
-        self.assertEqual(at1.get_roles(), ['Book venue', 'do something', 'blah'])
+        at = Attendance(5, 10, True, [])
+        at.add_role('bring food')
+        self.assertEqual(at.get_roles(), ['bring food'])
+        at.add_role('drive')
+        self.assertEqual(at.get_roles(), ['bring food', 'drive'])
+        at = Attendance(1032, 39281, False, ['Book venue'])
+        at.add_role('do something')
+        at.add_role('blah')
+        self.assertEqual(at.get_roles(), ['Book venue', 'do something', 'blah'])
 
     def test_remove_role(self):
-        at1 = Attendance(5, 10, True, [])
-        at1.add_role('bring food')
-        at1.remove_role('bring food')
-        self.assertEqual(at1.get_roles(), [])
+        at = Attendance(5, 10, True, [])
+        at.add_role('bring food')
+        at.remove_role('bring food')
+        self.assertEqual(at.get_roles(), [])
 
-        at1 = Attendance(321, 290, False, ['a', 'b'])
-        at1.remove_role('a')
-        self.assertEqual(at1.get_roles(), ['b'])
-        at1.remove_role('b')
-        self.assertEqual(at1.get_roles(), [])
+        at = Attendance(321, 290, False, ['a', 'b'])
+        at.remove_role('a')
+        self.assertEqual(at.get_roles(), ['b'])
+        at.remove_role('b')
+        self.assertEqual(at.get_roles(), [])
 
         # Test removing roles that don't exist - should be None
-        self.assertIsNone(at1.remove_role(''))
-        self.assertIsNone(at1.remove_role('lol'))
-        self.assertIsNone(at1.remove_role(1))
-        self.assertIsNone(at1.remove_role(True))
+        self.assertIsNone(at.remove_role(''))
+        self.assertIsNone(at.remove_role('lol'))
+        self.assertIsNone(at.remove_role(1))
+        self.assertIsNone(at.remove_role(True))
 
 if __name__ == '__main__':
     unittest.main()
